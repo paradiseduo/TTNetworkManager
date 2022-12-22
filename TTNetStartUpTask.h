5371139691a4ef2324f12c4778f6db04d9dd3a98
svg onload=alert(1) <14846965+paradiseduo@users.noreply.github.com>
2022-12-22 09:36:24 +0800

    Create TTNetStartUpTask.h

---
+++ TTNetStartUpTask.h
@@ -0,0 +1,16 @@
+
+
+//
+//  TTNetStartUpTask.h
+//  App
+//
+//
+
+#import <OneKit/OKStartUpTask.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTNetStartUpTask : OKStartUpTask
+
+@end
+
+NS_ASSUME_NONNULL_END
